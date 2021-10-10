6dd127de6eb32203023120d40cd59a35f09b4587
buggsboony <boonydeboon@gmail.com>
2021-10-10 18:54:49 -0400

    Bash error corrected

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ chmod +x $what$extension
 echo "Adding alias in .bashrc"
 
 echo  "### $what alias generated code at $(date +"%Y-%m-%d %H:%I:%S" )" >> $HOME/.bashrc
-echo  "$what( ) { source $PWD/$what.sh \$*; }" >> $HOME/.bashrc
+echo  "$what() { source $PWD/$what.sh \$*; }" >> $HOME/.bashrc
 echo  "### $what alias generated code " >> $HOME/.bashrc
 
 echo "done."
